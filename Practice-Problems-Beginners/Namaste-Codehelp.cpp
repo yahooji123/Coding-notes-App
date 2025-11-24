cb372e69d104935d64be4cf6ab77ebe76db16ae8
anurag-rastogi332 <rastogianurag675@gmail.com>
2025-11-24 19:07:57 +0530

    namaste codehelp

---
+++ Practice-Problems-Beginners/Namaste-Codehelp.cpp
@@ -0,0 +1,14 @@
+//Write your code here
+//https://www.codehelp.in/dashboard/courses/dsa-red/assignment-1/namaste-codehelp/description
+//platform-codehelp
+
+#include<iostream>
+using namespace std;
+int main(){
+   
+   cout<<"@@Namaste Codehelp@@";
+
+
+    return 0;
+}
+
