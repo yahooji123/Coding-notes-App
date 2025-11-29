4f3a0ffbd4b8b4bd16a13e0a1db11b524f4cae9f
anurag-rastogi332 <rastogianurag675@gmail.com>
2025-11-29 11:38:34 +0530

    Searching And Sorting

--- Practice-Problems-Beginners/Print Counting from N to 1.cpp
+++ Practice-Problems-Beginners/Print Counting from N to 1.cpp
@@ -1,3 +1,8 @@
+#include<iostream>
+using namespace std;
+
+
+
 class Solution {
 public:
     void printCountingReverse(int n) {
---
+++ Sorting-And Searching/Find Peak Index in a Mountain Array.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+
+Example:
+
+
+Input: arr = [0, 2, 5, 3, 1]
+Output: 2
+In this example, the peak element is 5, located at index 2.
+
+Note:
+
+The input is guaranteed to be a mountain array.
+Example 1:
+
+Input:
+
+Input: arr = [0, 1, 0]
+
+Output:
+
+1
+Explanation:
+
+Example 2:
+
+Input:
+
+Input: arr = [1, 3, 5, 4, 2]
+
+Output:
+
+2
+Explanation:
+
+Example 3:
+
+Input:
+
+Input: arr = [0, 2, 4, 6, 5, 3, 1]
+
+Output:
+
+3
+Explanation:
+
+*/
+
+int peakIndexInMountainArray(vector<int> &arr)
+{
+    // Your implementation here
+    int n = arr.size();
+    int s = 0;
+    int e = n - 1;
+    int ansIndex = -1;
+
+    while (s <= e)
+    {
+        int mid = (s + e) / 2;
+
+        if (arr[mid] < arr[mid + 1])
+        {
+            // right
+            s = mid + 1;
+        }
+        else
+        {
+            // mid is on line 2
+            // line 2 is also contains peak element
+            // mid may be on peak element or a non peak element
+            // stire and compute
+            ansIndex = mid;
+            e = mid - 1;
+        }
+    }
+    return ansIndex;
+}
