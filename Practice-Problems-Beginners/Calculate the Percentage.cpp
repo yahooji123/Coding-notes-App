5580616061e296ac39520f4b521b6c79a387eb39
anurag-rastogi332 <rastogianurag675@gmail.com>
2026-01-14 00:56:13 +0530

    Fix Render HTTPS session persistence

--- Practice-Problems-Beginners/Calculate the Percentage.cpp
+++
@@ -1,9 +0,0 @@
-
-
-class Solution {
-public:
-    double calculatePercentage(int totalMarks, int obtainedMarks) {
-        // Implement the logic to calculate the percentage
-        return (obtainedMarks*100/totalMarks);
-    }
-};
--- Practice-Problems-Beginners/Print Counting from 1 to N.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    void printCounting(int n) {
-        // Implement the loop logic to print numbers from 1 to n
-        for(int i=1;i<=n;i++){
-            cout<<i<<" ";
-        }
-    }
-};
